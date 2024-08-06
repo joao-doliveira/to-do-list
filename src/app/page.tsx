8ea5608fafb81a69fef10b8cfbae75990f8dcc8f
joaodoliveira-wolven <joao.doliveira@wolven.com.br>
2024-08-05 21:07:53 -0300

    feat: Navbar UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Trash2Icon } from "lucide-react";
-
 export default function Home() {
-  return (
-    <div className="p-10 flex gap-2">
-      <Button>
-        <Trash2Icon /> Test
-      </Button>
-      <Button variant="defaultOutline">
-        <Trash2Icon /> Test
-      </Button>
-      <Button variant="destructiveOutline">
-        <Trash2Icon /> Test
-      </Button>
-      <Button variant="secondary">
-        <Trash2Icon /> Test
-      </Button>
-      <Button variant="secondaryOutline">
-        <Trash2Icon /> Test
-      </Button>
-    </div>
-  );
+  return <div className="p-10 flex gap-2">content to be added</div>;
 }
